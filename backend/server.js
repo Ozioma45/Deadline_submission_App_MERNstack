e105a52d75dd9574262e3dfccb0a8c6fe800e4d0
Ozioma45 <oziomaegole@gmail.com>
2024-10-18 07:25:24 +0100

    coonecting the DB directly

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,23 @@ const dotenv = require("dotenv");
 const projectRoutes = require("./routes/projects");
 
 dotenv.config();
+
+console.log("Mongo URI:", process.env.MONGO_URI);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+console.log("Mongo URI:", process.env.MONGO_URI);
+
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://oziomaegole:Oziblink2846@submissionproject.fbcfi.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
