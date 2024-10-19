65730ddc34d2ff226063ce95132d5890acdf689a
Ozioma45 <oziomaegole@gmail.com>
2024-10-19 02:18:39 +0100

    changed the models/project file name

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Project = require("../models/Project");
+const Project = require("../models/project");
 
 // Submit a project
 router.post("/submit", async (req, res) => {
