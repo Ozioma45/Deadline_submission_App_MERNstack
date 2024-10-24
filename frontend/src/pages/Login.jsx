4c068e9c6850f7112ed00ab79d2b839865caa168
Ozioma45 <oziomaegole@gmail.com>
2024-10-24 19:23:48 +0100

    added the backend api to the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://deadline-submission-app-mernstack.onrender.com/api/auth/login",
         formData
       );
       localStorage.setItem("token", res.data.token);
